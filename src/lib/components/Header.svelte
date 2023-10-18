78959330115e33423bfb082c4803f5c11bf9a6bf
Sharath <sharath.dt@gmail.com>
2023-10-18 17:26:53 +0530

    remove showmenu implementation

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,17 +1,10 @@
 <script>
   import { FIRST_NAME, LAST_NAME } from "$lib/constants";
-  import { isPhone } from "$lib/stores";
-
-  $: showMenu = $isPhone;
 </script>
 
-<nav
-  class="grid place-items-center pt-12 md:pt-20 pb-6 md:pb-12"
-  on:mouseleave={() => (showMenu = false)}
->
+<nav class="grid place-items-center pt-12 md:pt-20 pb-6 md:pb-12">
   <a href="/" title="Home">
     <button
-      on:mouseenter={() => (showMenu = true)}
       class="border rounded-full border-4 hover:border-orange-600 hover:bg-orange-200 duration-300"
     >
       <img
