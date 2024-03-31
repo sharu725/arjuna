49f92670a1625d8525e3fbe39c64bad3e282fece
sharath <sharath.dt@gmail.com>
2024-03-31 09:30:42 +0530

    udpate packages

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,13 +1,15 @@
 export const SITE_TITLE = "Arjuna";
 export const FIRST_NAME = "Pānduputra";
 export const LAST_NAME = "Arjuna";
+
+// dev purpose
+// change it to your repo
+export const repoLink = "https://github.com/sharu725/arjuna";
+
 export const navs = [
   { title: "Home", href: "/" },
+  { title: "Github", href: repoLink },
   { title: "About Us", href: "/about" },
   { title: "Terms & Conditions", href: "/terms" },
 ];
 export const copyrightsBy = { title: "webjeda", url: "https://webjeda.com" };
-
-// dev purpose
-// change it to your repo
-export const repoLink = "https://github.com/sharu725/arjuna"
